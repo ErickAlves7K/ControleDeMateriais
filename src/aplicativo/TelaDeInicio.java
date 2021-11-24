09c2bf7bae260821328f0ce994eeae5019063e5a
Anubis7K <erickhue1@hotmail.com>
2021-11-24 00:03:15 -0300

    Finalizando tela inicial

--- src/aplicativo/TelaDeInicio.java
+++ src/aplicativo/TelaDeInicio.java
@@ -15,13 +15,12 @@ public class TelaDeInicio extends JFrame implements ActionListener{
 		setTitle("Início");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		getContentPane().setBackground(Color.decode("#1E5128"));
+		getContentPane().setBackground(Color.white);
 		setResizable(false);
 		setVisible(true);
 		
 		entrar = new JButton("Entrar");
-		entrar.setBounds(350, 550, 350, 50);
-		entrar.setBackground(Color.decode("#4E9F3D"));
+		entrar.setBackground(Color.decode("#1E5128"));
 		entrar.setForeground(Color.decode("#D8E9A8"));
 		entrar.setBorderPainted(false);
 		entrar.addActionListener(this);
@@ -29,16 +28,18 @@ public class TelaDeInicio extends JFrame implements ActionListener{
 		entrar.setFont(new Font("Roboto", Font.PLAIN, 24));
 		
 		bemvindo = new JLabel("Bem-Vindo");
-		bemvindo.setBounds(420, 100, 400, 50);
 		bemvindo.setFont(new Font("Roboto", Font.PLAIN, 48));
-		bemvindo.setForeground(Color.decode("#D8E9A8"));
+		bemvindo.setForeground(Color.decode("#1E5128"));
 		
-		icone = new JLabel();
-		icone.setBounds(0, 0, WIDTH, HEIGHT);
+		icone = new JLabel(new ImageIcon(getClass().getResource("controle.jfif")));
+		icone.setVisible(true);
 		
 		add(entrar);
 		add(bemvindo);
 		add(icone);
+		entrar.setBounds(350, 550, 350, 50);
+		bemvindo.setBounds(420, 100, 400, 50);
+		icone.setBounds(350, 160, 350, 350);
 	}
 	
 	public void actionPerformed(ActionEvent e) {
